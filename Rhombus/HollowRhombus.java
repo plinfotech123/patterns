073eea1f927e87ce53d4a1db25b2ea01ac02d5d7
plinfotech123 <Prabesh0@}>
2023-12-29 09:42:20 -0600

    Rhombus:Solid and hollow

---
+++ Rhombus/HollowRhombus.java
@@ -0,0 +1,25 @@
+package patterns.Rhombus;
+
+public class HollowRhombus {
+    public static void main(String[] args) {
+        int n=7;
+        // outer
+        for(int i=1; i<=n; i++){
+            // inner loop
+            // for the space
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            // for the start
+            for(int j=1; j<=n; j++){
+                // for the hollow rhombus
+                if(i==1 || j==1 || i==n || j==n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
